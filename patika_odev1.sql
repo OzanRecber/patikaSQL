f6f00bc1821475e1f35e9384c443ad60b7dd0404
Ozan Recber <121625150+OzanRecber@users.noreply.github.com>
2023-04-19 19:28:01 +0300

    Update patika_odev1.sql

--- patika_odev1.sql
+++ patika_odev1.sql
@@ -4,7 +4,7 @@ SELECT title, description FROM film;
 --film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length)
 --60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
 SELECT * FROM film
-WHERE length > 60 AND length > 75;
+WHERE length > 60 AND length < 75;
 
 --film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE
 --replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
